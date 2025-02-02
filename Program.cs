121614905f588e06ce48fceec77cc9d82e35b075
Zasadar <zasada.r89@gmail.com>
2025-02-02 15:24:41 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Modul_5_5_3
+{
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Напишите чонибудь");
+            var str = Console.ReadLine();
+
+            Console.WriteLine("Укажите глубину эха");
+            var deep = int.Parse(Console.ReadLine());
+
+            Echo(str, deep);
+
+
+            Console.ReadKey();
+        }
+
+        static void Echo(string saidword, int deep)
+        {
+            var modif = saidword;
+            if (modif.Length > 2)
+            {
+                modif = modif.Remove(0, 2);
+            }
+            Console.BackgroundColor = (ConsoleColor)deep;
+            Console.WriteLine("..." + modif);
+
+            if (deep > 1)
+            {
+                Echo(modif, deep - 1);
+            }
+
+        }
+    }
+}
